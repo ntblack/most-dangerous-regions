9a2e310c31aeae5f2de44360803581c3ff3c342d
Nate Black <nathanael.black@gmail.com>
2016-08-21 11:15:14 -0700

    wire things together in index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 #! /usr/bin/env node
 const path = require('path');
-const nameRegionMapping = require(path.join(__dirname, 'lib', 'name-regions'));
+const mappingFunction = require(path.join(__dirname, 'lib', 'name-regions'));
+const regionMapper = require(path.join(__dirname, 'lib', 'region-mapper'));
+const features = require(path.join(__dirname, 'data', 'all_month.json')).features.slice(0, 1);
 
-const place = nameRegionMapping({properties: {place: "San Francisco, CA"}});
-console.log(`hello ${place}`);
+const mappedData = regionMapper(features, mappingFunction);
+
+// ES6 Map cannot be converted to JSON because the keys may be any object, while JSON spec requires keys
+// to be of type string. Therefore we can't just use JSON.stringify to debug.
+for(let [key, val] of mappedData) {
+    console.log("%j: %j", key, val);
+}
+
 
