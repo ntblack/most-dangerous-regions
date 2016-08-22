85b1ad59a5d3e83194f628d2f91b32253a80c154
Nate Black <nathanael.black@gmail.com>
2016-08-22 09:09:55 -0700

    add country region mapping

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 #! /usr/bin/env node
 const path = require('path');
-const mappingFunction = require(path.join(__dirname, 'lib', 'name-regions'));
+const mappingFunction = require(path.join(__dirname, 'lib', 'country-regions'));
 const regionMapper = require(path.join(__dirname, 'lib', 'region-mapper'));
-const features = require(path.join(__dirname, 'data', 'all_month.json')).features.slice(0, 1);
+const sum = require(path.join(__dirname, 'lib', 'sum-richter'));
+const features = require(path.join(__dirname, 'data', 'all_month.json')).features;
 
+const N = 10;
 const mappedData = regionMapper(features, mappingFunction);
+const topN = [];
+for(let [regionName, features] of mappedData) {
 
-// ES6 Map cannot be converted to JSON because the keys may be any object, while JSON spec requires keys
-// to be of type string. Therefore we can't just use JSON.stringify to debug.
-for(let [key, val] of mappedData) {
-    console.log("%j: %j", key, val);
+    const entry = {region: regionName, earthquakeCount: features.length, totalPower: sum(features)}
+    if (topN.length < N) {
+        topN.push(entry);
+    } else {
+        const weakest = topN.pop();
+        if(entry.totalPower > weakest) {
+            topN.push(entry);
+        } else {
+            topN.push(weakest);
+        }
+    }
+
+    topN.sort((a, b) => a.totalPower < b.totalPower);
 }
+
+console.log(JSON.stringify(topN));
 
 
